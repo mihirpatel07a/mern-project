60b7cc171893e760442b8ee80e598141b584d82b
mihir <mihirjetpariya2003@gmail.com>
2024-01-09 23:37:28 +0530

    till redux toolkit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,51 +1,65 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-    // destructruring data from request
-    const { username, email, password} = req.body;
+  var user = await User.findOne({ email: req.body.email });
 
-    // hashing the password
-    const hashedPassword = bcryptjs.hashSync(password, 10); 
+  if (user) {
+    return res
+      .status(200)
+      .json({ message: "User already Exist", success: false });
+  }
+  const username = req.body.username;
 
-    // create new user
-    const newUser = new User({username, email, password: hashedPassword});
+  const email = req.body.email;
+  const pass = req.body.password;
 
-    try{
-        await newUser.save();
-        res.status(201).json('User Created Successfully');
-    }
-    catch(error){
-        next(error);
-    }
-}
+  // hashing the password
+  const hashedPassword = bcryptjs.hashSync(pass, 10);
 
+  // create new user
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.status(200).send({ message: "Registration successful", success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      meaasge: `Register Controller ${error.message}`,
+    });
+   
+  }
+};
 
 export const signin = async (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try{
-        const validUser = await User.findOne({ email });
+  try {
+    const validUser = await User.findOne({ email });
 
-        // Check that user with email exists or not
-        if(!validUser) return next(errorHandler(404, 'User Not Found!'));
+    // Check that user with email exists or not
+    if (!validUser) return next(errorHandler(404, "User Not Found!"));
 
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
 
-        // Check password is correct or not
-        if(!validPassword) return next(errorHandler(401, 'Wrong Credentials!'));
+    // Check password is correct or not
+    if (!validPassword) return next(errorHandler(401, "Wrong Password!"));
 
-        // If both email and password are correct then we create token
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // If both email and password are correct then we create token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
-        const { password: pass, ...rest } = validUser._doc;
+    const { password: pass, ...rest } = validUser._doc;
 
-        // Save token as cookie
-        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
-    }
-    catch(error){
-        next(error);
-    }
-}
+    // Save token as cookie
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers( {user: userReducer});
+
+const persistconfig = {
+   key : "root", 
+   storage, 
+   version : 1
+}
+
+const persistreducer = persistReducer(persistconfig , rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistreducer,
 
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false
   }),
 })
+
+export const persistor = persistStore(store);
