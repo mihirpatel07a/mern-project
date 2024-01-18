c0c35c95dab4120f2d28f5a82372635eb148c092
mihir <mihirjetpariya2003@gmail.com>
2024-01-18 23:52:29 +0530

    add create listing

---
+++ backend/controllers/listing.controller.js
@@ -0,0 +1,16 @@
+import Listing  from "../models/listing.model.js";
+
+
+export const createListing = async(req , res , next)=> {
+
+    try{
+      const listing = await Listing.create(req.body);
+      console.log(listing);
+      res.status(201).json(listing);
+    }
+    
+    catch(error)
+    {
+        next(error);
+    }
+}
